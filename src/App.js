529c44d407c60341e7baba5b70f24302bb76596e
Girum Yigezu <girum32@gmail.com>
2021-01-17 19:41:22 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Banner from './Components/Banner';
+import Nav from './Components/Nav';
+import Row from './Components/Row';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+    </div>
+  );
+}
+
+export default App;
